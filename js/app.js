885c0fc23f0de317ae42c68f499577548fc2a628
luis rodriguez <kike.lerj2710@gmail.com>
2021-09-27 17:36:12 -0500

    mostar el que el formulario no este vacio

--- js/app.js
+++ js/app.js
@@ -1,17 +1,34 @@
 //===== variables=====
 const btnEnviar = document.querySelector('#enviar');
+//variables de los campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
 
 //====== evento =======
 eventListener();
 function eventListener() {
+    // Iniciar ó cargar la App
     document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    //campos del formulario
+    email.addEventListener('blur', validarFormulario);// recoradar blur me funciona para cuando quiera salir de un formulario no este vacio
+    asunto.addEventListener('blur', validarFormulario);// recoradar blur me funciona para cuando quiera salir de un formulario no este vacio
+    mensaje.addEventListener('blur', validarFormulario);// recoradar blur me funciona para cuando quiera salir de un formulario no este vacio
 }
 
 //======== funciones =========
 
 function iniciarApp() {
-    btnEnviar.disabled = true;
-    btnEnviar.classList.add( 'cursor-not-allowed',' opacity-50');
+btnEnviar.disabled = true;
+btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
 }
 
 
+function validarFormulario(e){// validar que no este vacio el formulario
+    if(e.target.value.length > 0){
+        e.target.classList.add('border-green-500','border');
+    }else{
+        e.target.classList.add('border-red-500','border');
+    }
+}
