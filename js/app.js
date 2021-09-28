eb15720e164c6abc767571a22e792154b4e8a670
luis rodriguez <kike.lerj2710@gmail.com>
2021-09-27 19:01:18 -0500

    validar email y terminar de validar los formularios

--- js/app.js
+++ js/app.js
@@ -28,17 +28,38 @@ btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
 
 function validarFormulario(e){// validar que no este vacio el formulario
     if(e.target.value.length > 0){
+        //  Elimina los errores
+        const error = document.querySelector('p.error');
+            error.remove();
+        e.target.classList.remove('border-red-500','border');
         e.target.classList.add('border-green-500','border');
     }else{
+        e.target.classList.remove('border-green-500','border');
         e.target.classList.add('border-red-500','border');
-        mostrarError();
+
+            mostrarError('todos los campos son obligatorio');
+    }
+    if(e.target.type === 'email'){
+        //expresion regular
+        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        // const resultado = e.target.value.indexOf('@'); //validacion regular
+        if(er.test(e.target.value)){            
+            const error = document.querySelector('p.error');
+               error.remove();
+            e.target.classList.remove('border-red-500','border');   
+            e.target.classList.add('border-green-500','border');    
+        }else{
+            e.target.classList.remove('border-green-500','border');
+            e.target.classList.add('border-red-500','border');
+            mostrarError('Email no valido');;
+        }
     }
 }
 
 //==== Mostrar mensaje Error===
-function mostrarError() {
+function mostrarError(menssage) {
     const mensajeError = document.createElement('p');
-    mensajeError.textContent='todos los campos son obligatorio';
+    mensajeError.textContent = menssage ;
     mensajeError.classList.add('border-red-500','border','background-red-100', 'text-red-500', 'text-center','p-3', 'mt-5','error');
 
 
